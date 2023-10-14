2a8801d987429094fa5e024a55f2735fa7d91241
marchewazz <mateuszooospot@gmail.com>
2023-10-14 19:17:20 +0200

    Overflow x hidden on body

--- src/App.tsx
+++ src/App.tsx
@@ -102,7 +102,7 @@ function AppLayout() {
 
   return (
     <div className={`${darkMode && "dark"}`}>
-      <div className="bg-dark-green dark:bg-earie-black min-h-screen transition-all duration-300">
+      <div className="bg-dark-green dark:bg-earie-black min-h-screen transition-all duration-300 overflow-x-hidden">
         <NavBar userData={userData} isUserLogged={isUserLogged} logoutFunction={logout} onlyNonLoggedPaths={onlyNonLoggedPaths} darkMode={darkMode} darkModeChangeFunction={changeDarkMode} width={width} height={height} />
         <Outlet context={{socket, userData, isUserLogged, userDataReady, darkMode}} />
         <PopUpContainer socket={socket} />
