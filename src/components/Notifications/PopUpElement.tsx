e3c10e7c8e74c4e8163105a5adc71f0d41be9ff7
marchewazz <mateuszooospot@gmail.com>
2023-11-01 17:48:45 +0100

    Separate pop up component

---
+++ src/components/Notifications/PopUpElement.tsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+import PopUpMessage from "../../models/PopUpMessage.model";
+import strings from "../../utilities/strings";
+
+export default function PopUpElement(props: { notification: PopUpMessage}) {
+
+    const successMessages: string[] = ["rented", "rentalCancelled", "moneyAdded", "addedToFavorites", "removedFromFavorites", "invitationSent", "invitationReceived", "profileEdited", "nickTaken"];
+    const errorMessages: string[] = ["errorMessage", "noMoney"];    
+
+    return (
+        <div className={`${successMessages.includes(props.notification.message) ? "notification-success" : ""} 
+          ${errorMessages.includes(props.notification.message) ? "notification-error" : ""}`
+          }>
+            { props.notification.message === "invitationReceived" ? (
+              <p>
+                { strings.formatString(strings.popUpNotifications.invitationReceived, { senderNick: props.notification.senderNick })}
+                <Link to={`/user/${props.notification.senderID}`}>
+                  { strings.popUpNotifications.viewProfile}
+                </Link>
+              </p>
+            ) : (
+              <p>
+                { strings.popUpNotifications[props.notification.message as keyof typeof strings.popUpNotifications] || ""}
+              </p>
+            )}
+          </div>
+    )
+}
