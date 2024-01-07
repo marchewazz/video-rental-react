e12802e051b33eaad479752597cc76dee0b261fe
marchewazz <mateuszooospot@gmail.com>
2024-01-07 14:19:49 +0100

    Language setter flags border radius

--- src/components/LanguageSetter.tsx
+++ src/components/LanguageSetter.tsx
@@ -11,10 +11,10 @@ export default function LanguageSetter() {
     return (
         <div className="p-4 gap-4 flex lg:gap-2 lg:p-0">
             <button className={`${strings.getLanguage() != "pl" ? "grayscale" : ""} hover:grayscale-0 transition-all duration-300 ease-in-out`} onClick={() => changeLanguage("pl")}>
-                <img src="https://flagsapi.com/PL/shiny/32.png" />
+                <img className="rounded-full" src="https://flagsapi.com/PL/shiny/32.png" />
             </button>
             <button className={`${strings.getLanguage() != "en" ? "grayscale" : ""} hover:grayscale-0 transition-all duration-300 ease-in-out`} onClick={() => changeLanguage("en")}>
-                <img src="https://flagsapi.com/US/shiny/32.png" />
+                <img className="rounded-full" src="https://flagsapi.com/US/shiny/32.png" />
             </button>
         </div>
       
